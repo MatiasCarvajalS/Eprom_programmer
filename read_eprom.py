fec8a3c3f124f71adc9705579099b896f4973295
Matias Carvajal <mncarvaj1@gmail.com>
2019-05-01 13:39:41 -0400

    Routine to read the Eprom 27c801 and write to file in binary

---
+++ read_eprom.py
@@ -0,0 +1,17 @@
+import serial
+import time
+size_eprom = 1048576
+
+ser = serial.Serial('COM3')             # open serial port
+print('Reading from', ser.name)         # check which port was really used
+print('Waiting 3 secs to correct initiate ARDUINO')
+time.sleep(3)
+
+ser.write('r'.encode('utf-8')) #send the signal to start reading
+a = ser.read(size_eprom)       # read eprom completely
+ser.close()                    # close port
+
+print("Correct Reading")
+with open("eprom_data",'wb') as f: #write file, overwrite file
+    f.write(a)
+print("Correct Writing to file")
